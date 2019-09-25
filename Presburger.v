18fb3662599f1a55bd5755fb7798ad3e6fec62ae
mfehr <mathieu.fehr@gmail.com>
2019-09-25 12:54:24 +0200

    Move some files and add Presburger Set class.

---
+++ Presburger.v
@@ -0,0 +1,17 @@
+From Coq Require Import ssreflect ssrfun ssrbool.
+
+Require Import String.
+Open Scope string_scope.
+
+Class PolySetImpl (s: Type) :=
+  {
+    eval_point : s -> (string -> nat) -> bool;
+
+    union_set : s -> s -> s;
+    union_set_spec : forall p1 p2 x,
+        eval_point (union_set p1 p2) x = eval_point p1 x || eval_point p2 x;
+
+    intersect_set : s -> s -> s;
+    intersect_set_spec : forall p1 p2 x,
+        eval_point (intersect_set p1 p2) x = eval_point p1 x && eval_point p2 x;
+  }.
