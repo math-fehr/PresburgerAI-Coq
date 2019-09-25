1ed7afb3b9216eb51c66248e3a5993199e383a59
mfehr <mathieu.fehr@gmail.com>
2019-09-25 14:15:29 +0200

    Define Presburger sets as an abstract domain.

--- AbstractDomain.v
+++ AbstractDomain.v
@@ -1,6 +1,6 @@
-Require Export PolyAI.TotalMap.
-Require Export Coq.Sets.Ensembles.
 Require Import PolyAI.SSA.
+Require Export PolyAI.Presburger.
+Require Export Coq.Sets.Ensembles.
 Require Import Coq.Program.Equality.
 Require Import Coq.Lists.List.
 From Coq Require Import ssreflect ssrfun ssrbool.
@@ -12,17 +12,59 @@ Open Scope string_scope.
 Class adom (ab:Type) :=
   {
     le : ab -> ab -> bool;
+    bot : ab;
     top : ab;
     join : ab -> ab -> ab;
 
+    gamma : ab -> Ensemble RegisterMap;
+
     le_refl: forall a, le a a = true;
     le_trans: forall a1 a2 a3, le a1 a2 = true -> le a2 a3 = true -> le a1 a3 = true;
-    gamma : ab -> Ensemble RegisterMap;
     gamma_monotone : forall a1 a2, le a1 a2 = true -> Included RegisterMap (gamma a1) (gamma a2);
     gamma_top : forall x, Ensembles.In RegisterMap (gamma top) x;
     join_sound_l : forall a1 a2, le a1 (join a1 a2);
     join_sound_r : forall a1 a2, le a2 (join a1 a2);
   }.
+
+
+(* Instance for the polyhedral abstract domain *)
+
+Theorem gamma_presburger_monotone {s: Type} (P : PresburgerSet s) :
+  forall p1 p2, is_subset p1 p2 = true ->
+           Included (string -> nat)
+                    (fun x => eval_point p1 x = true)
+                    (fun x => eval_point p2 x = true).
+Proof.
+  move => p1 p2 /is_subset_spec Hsubset x Hin.
+  rewrite /Ensembles.In in Hin *.
+  by apply Hsubset, Hin.
+Qed.
+
+Theorem gamma_presburger_top {s: Type} (P : PresburgerSet s) :
+  forall x, Ensembles.In (string -> nat) (eval_point universe_set) x.
+Proof.
+  move => x.
+  rewrite /Ensembles.In.
+  apply universe_set_spec.
+Qed.
+
+Instance PresburgerSetAD {s: Type} (P : PresburgerSet s) : adom s :=
+  {
+    le := is_subset;
+    bot := empty_set;
+    top := universe_set;
+    join := union_set;
+
+    gamma := eval_point;
+
+    le_refl := is_subset_refl P;
+    le_trans := is_subset_trans P;
+    gamma_monotone := gamma_presburger_monotone P;
+    gamma_top := gamma_presburger_top P;
+    join_sound_l := is_subset_union_l P;
+    join_sound_r := is_subset_union_r P;
+  }.
+
 
 (* Transfer functions for our language *)
 Class transfer_function {ab: Type} (A: adom ab) :=
--- Presburger.v
+++ Presburger.v
@@ -1,4 +1,5 @@
 From Coq Require Import ssreflect ssrfun ssrbool.
+Require Export Coq.Sets.Ensembles.
 
 Require Import String.
 Open Scope string_scope.
@@ -7,6 +8,12 @@ Class PresburgerSet (s: Type) :=
   {
     eval_point : s -> (string -> nat) -> bool;
 
+    empty_set : s;
+    empty_set_spec : forall x, eval_point empty_set x = false;
+
+    universe_set : s;
+    universe_set_spec : forall x, eval_point universe_set x = true;
+
     union_set : s -> s -> s;
     union_set_spec : forall p1 p2 x,
         eval_point (union_set p1 p2) x = eval_point p1 x || eval_point p2 x;
@@ -14,4 +21,45 @@ Class PresburgerSet (s: Type) :=
     intersect_set : s -> s -> s;
     intersect_set_spec : forall p1 p2 x,
         eval_point (intersect_set p1 p2) x = eval_point p1 x && eval_point p2 x;
+
+    is_subset : s -> s -> bool;
+    is_subset_spec : forall p1 p2, is_subset p1 p2 = true <-> forall x, eval_point p1 x = true -> eval_point p2 x = true;
   }.
+
+
+Theorem is_subset_refl {s: Type} (P : PresburgerSet s) :
+  forall p, is_subset p p = true.
+Proof.
+  move => p.
+  apply is_subset_spec => //.
+Qed.
+
+Theorem is_subset_trans {s: Type} (P : PresburgerSet s) :
+  forall p1 p2 p3, is_subset p1 p2 = true ->
+              is_subset p2 p3 = true ->
+              is_subset p1 p3 = true.
+Proof.
+  move => p1 p2 p3 /is_subset_spec H12 /is_subset_spec H23.
+  apply is_subset_spec.
+  by auto.
+Qed.
+
+Theorem is_subset_union_l {s: Type} (P: PresburgerSet s ) :
+  forall p1 p2, is_subset p1 (union_set p1 p2).
+Proof.
+  move => p1 p2.
+  apply is_subset_spec => x Hp1.
+  rewrite union_set_spec Hp1.
+  reflexivity.
+Qed.
+
+Theorem is_subset_union_r {s: Type} (P: PresburgerSet s ) :
+  forall p1 p2, is_subset p2 (union_set p1 p2).
+Proof.
+  move => p1 p2.
+  apply is_subset_spec => x Hp2.
+  rewrite union_set_spec Hp2.
+  by apply orbT.
+Qed.
+
+
