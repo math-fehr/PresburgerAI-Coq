3cd53caa53d8f4ceeef3d3e9c7ad27ac8d401b12
mfehr <mathieu.fehr@gmail.com>
2019-10-19 01:41:37 +0200

    Add comment on ssrZ.

--- src/ssrZ.v
+++ src/ssrZ.v
@@ -2,5 +2,7 @@ Local Set Warnings "-notation-overridden".
 From mathcomp Require Export eqtype.
 Require Export Coq.ZArith.BinInt.
 
+(* Define canonical structure for Z with equality *)
+
 Canonical Z_eqMixin := EqMixin Z.eqb_spec.
 Canonical Z_eqType := Eval hnf in EqType Z Z_eqMixin.
