e5f64bc2fa7f35545a6c16bb260a4f48d295c613
mfehr <mathieu.fehr@gmail.com>
2019-11-15 15:52:20 +0100

    Add compose_relation_le.

--- src/RelationalAbstractDomain.v
+++ src/RelationalAbstractDomain.v
@@ -1,3 +1,4 @@
+From Coq Require Import ssreflect ssrfun ssrbool.
 Require Export Coq.Sets.Ensembles.
 From Coq Require Import ssrbool.
 From PolyAI Require Export AbstractDomain.
@@ -22,3 +23,16 @@ Class adom_relational {concrete_state abstract_state: eqType}
     transitive_closure_ge_id : forall a, le id_relation (transitive_closure a);
     transitive_closure_eq_compose : forall a, le (compose_relation a (transitive_closure a)) (transitive_closure a);
   }.
+
+Theorem compose_relation_le {concrete_state abstract_state: eqType}
+        {A: adom (prod_eqType concrete_state concrete_state) abstract_state} (AR: adom_relational A)
+  (a1 a2 comp_a: abstract_state):
+  le a1 a2 -> le (compose_relation comp_a a1) (compose_relation comp_a a2).
+Proof.
+  move => /gamma_monotone Hle.
+  apply gamma_monotone => [[x0 x2]] Hin1.
+  apply compose_relation_spec in Hin1.
+  move: Hin1 => [x1 [Hin1 Hin2]].
+  apply compose_relation_spec.
+  eauto.
+Qed.
