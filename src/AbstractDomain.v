749f26cac04b75e457d767997bde44f0d7ecd64c
mfehr <mathieu.fehr@gmail.com>
2019-11-18 22:24:35 +0100

    Add join spec.

--- src/AbstractDomain.v
+++ src/AbstractDomain.v
@@ -17,6 +17,9 @@ Class adom (concrete_state abstract_state: eqType) :=
     le_trans: forall a1 a2 a3, le a1 a2 -> le a2 a3 -> le a1 a3;
     gamma_monotone : forall a1 a2, le a1 a2 <-> Included concrete_state (gamma a1) (gamma a2);
     gamma_top : forall x, Ensembles.In concrete_state (gamma top) x;
+    join_spec : forall a1 a2 x, Ensembles.In concrete_state (gamma (join a1 a2)) x <->
+                         Ensembles.In concrete_state (gamma a1) x \/
+                         Ensembles.In concrete_state (gamma a2) x;
     join_sound_l : forall a1 a2, le a1 (join a1 a2);
     join_sound_r : forall a1 a2, le a2 (join a1 a2);
   }.
