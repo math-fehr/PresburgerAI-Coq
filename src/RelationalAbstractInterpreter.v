c3268605c00f62b44692448fe4940e602d0e5bf3
mfehr <mathieu.fehr@gmail.com>
2019-11-27 12:32:21 +0100

    Renamed two files.

--- src/RelationalAbstractInterpreter.v
+++ src/RelationalAbstractInterpreter.v
@@ -1,7 +1,7 @@
 From Coq Require Import ssreflect ssrfun ssrbool.
 Local Set Warnings "-notation-overridden".
 From mathcomp Require Import ssrnat.
-From PolyAI Require Export LRTransferFunction LSSA Tactic.
+From PolyAI Require Export RelationalTransferFunction LSSA Tactic.
 From Coq Require Import Lists.List.
 From mathcomp.ssreflect Require Import seq.
 
