6c3c6a5e2dbd2c102c4da866ecb5b4bae94c97d4
mfehr <mathieu.fehr@gmail.com>
2019-11-27 13:02:47 +0100

    Add Presburger maps.

--- src/Presburger.v
+++ src/Presburger.v
@@ -1,5 +1,5 @@
 From Coq Require Import ssreflect ssrfun ssrbool.
-From PolyAI Require Export TotalMap ssrZ ssrstring.
+From PolyAI Require Export TotalMap ssrZ ssrstring Tactic.
 Require Export Coq.Sets.Ensembles.
 Require Export Coq.ZArith.BinInt.
 
@@ -40,34 +40,59 @@ Fixpoint eval_aff (a: Aff) (m: string -> Z) :=
 
 (* Implementation of a Presburger set / pw_aff library *)
 
-Class PresburgerImpl (PSet PwAff: Type) :=
+Class PresburgerImpl (PMap PSet PwAff: Type) :=
   {
-    eval_set : PSet -> (string -> Z) -> bool;
-    eval_set_same : forall m1 m2,
-        (forall x, m1 x = m2 x) -> forall s, eval_set s m1 = eval_set s m2;
+    eval_pset : PSet -> (string -> Z) -> bool;
+    eval_pset_same : forall m1 m2,
+        (forall x, m1 x = m2 x) -> forall s, eval_pset s m1 = eval_pset s m2;
 
     empty_set : PSet;
-    empty_set_spec : forall x, ~~(eval_set empty_set x);
+    empty_set_spec : forall x, ~~(eval_pset empty_set x);
 
     universe_set : PSet;
-    universe_set_spec : forall x, eval_set universe_set x;
+    universe_set_spec : forall x, eval_pset universe_set x;
 
     union_set : PSet -> PSet -> PSet;
     union_set_spec : forall p1 p2 x,
-        eval_set (union_set p1 p2) x = eval_set p1 x || eval_set p2 x;
+        eval_pset (union_set p1 p2) x = eval_pset p1 x || eval_pset p2 x;
 
     intersect_set : PSet -> PSet -> PSet;
     intersect_set_spec : forall p1 p2 x,
-        eval_set (intersect_set p1 p2) x = eval_set p1 x && eval_set p2 x;
+        eval_pset (intersect_set p1 p2) x = eval_pset p1 x && eval_pset p2 x;
 
     is_subset : PSet -> PSet -> bool;
     is_subset_spec : forall p1 p2, is_subset p1 p2 <->
-                              forall x, eval_set p1 x -> eval_set p2 x;
+                              forall x, eval_pset p1 x -> eval_pset p2 x;
 
     set_project_out : PSet -> string -> PSet;
-    set_project_out_spec : forall p d (m: total_map), eval_set (set_project_out p d) m <->
-                                    exists v, eval_set p (d !-> v; m);
+    set_project_out_spec : forall p d (m: total_map), eval_pset (set_project_out p d) m <->
+                                    exists v, eval_pset p (d !-> v; m);
 
+    eval_pmap : PMap -> (string -> string -> Z) -> bool;
+    eval_pmap_same : forall m1 m2,
+        (forall x y, m1 x y = m2 x y) -> forall s, eval_pmap s m1 = eval_pmap s m2;
+
+    empty_map : PMap;
+    empty_map_spec : forall x, ~~(eval_pmap empty_map x);
+
+    universe_map : PMap;
+    universe_map_spec : forall x, eval_pmap universe_map x;
+
+    union_map : PMap -> PMap -> PMap;
+    union_map_spec : forall p1 p2 x,
+        eval_pmap (union_map p1 p2) x = eval_pmap p1 x || eval_pmap p2 x;
+
+    intersect_map : PMap -> PMap -> PMap;
+    intersect_map_spec : forall p1 p2 x,
+        eval_pmap (intersect_map p1 p2) x = eval_pmap p1 x && eval_pmap p2 x;
+
+    is_subset_map : PMap -> PMap -> bool;
+    is_subset_map_spec : forall p1 p2, is_subset_map p1 p2 <->
+                                  forall x, eval_pmap p1 x -> eval_pmap p2 x;
+
+    map_project_out : PMap -> string -> PMap;
+    map_project_out_spec : forall p d (m: total_map), eval_pmap (map_project_out p d) m <->
+                                                 exists v, eval_pmap p (d !-> v; m);
 
     eval_pw_aff : PwAff -> (string -> Z) -> option Z;
 
@@ -76,7 +101,7 @@ Class PresburgerImpl (PSet PwAff: Type) :=
 
     intersect_domain : PwAff -> PSet -> PwAff;
     intersect_domain_spec : forall p s x, eval_pw_aff (intersect_domain p s) x =
-                                     if eval_set s x then
+                                     if eval_pset s x then
                                        eval_pw_aff p x
                                      else
                                        None;
@@ -89,21 +114,21 @@ Class PresburgerImpl (PSet PwAff: Type) :=
                                    end;
 
     eq_set : PwAff -> PwAff -> PSet;
-    eq_set_spec : forall p1 p2 x, eval_set (eq_set p1 p2) x =
+    eq_set_spec : forall p1 p2 x, eval_pset (eq_set p1 p2) x =
                              match (eval_pw_aff p1 x, eval_pw_aff p2 x) with
                              | (Some v1, Some v2) => v1 == v2
                              | _ => false
                              end;
 
     ne_set : PwAff -> PwAff -> PSet;
-    ne_set_spec : forall p1 p2 x, eval_set (ne_set p1 p2) x =
+    ne_set_spec : forall p1 p2 x, eval_pset (ne_set p1 p2) x =
                              match (eval_pw_aff p1 x, eval_pw_aff p2 x) with
                              | (Some v1, Some v2) => (v1 != v2)
                              | _ => false
                              end;
 
     le_set : PwAff -> PwAff -> PSet;
-    le_set_spec : forall p1 p2 x, eval_set (le_set p1 p2) x =
+    le_set_spec : forall p1 p2 x, eval_pset (le_set p1 p2) x =
                              match (eval_pw_aff p1 x, eval_pw_aff p2 x) with
                              | (Some v1, Some v2) => v1 <=? v2
                              | _ => false
@@ -111,7 +136,7 @@ Class PresburgerImpl (PSet PwAff: Type) :=
 
     indicator_function : PSet -> PwAff;
     indicator_function_spec : forall s x, eval_pw_aff (indicator_function s) x =
-                                     if eval_set s x then
+                                     if eval_pset s x then
                                        Some 1
                                      else
                                        Some 0;
@@ -119,14 +144,28 @@ Class PresburgerImpl (PSet PwAff: Type) :=
 
 Generalizable All Variables.
 
-Theorem is_subset_refl `{PI: PresburgerImpl PSet PwAff}:
+Theorem empty_set_spec_rw `{PI: PresburgerImpl PMap PSet PwAff}:
+  forall x, eval_pset empty_set x = false.
+Proof.
+  move => x.
+  by rewrite (negbTE (empty_set_spec _)).
+Qed.
+
+Theorem empty_map_spec_rw `{PI: PresburgerImpl PMap PSet PwAff}:
+  forall x, eval_pmap empty_map x = false.
+Proof.
+  move => x.
+  by rewrite (negbTE (empty_map_spec _)).
+Qed.
+
+Theorem is_subset_refl `{PI: PresburgerImpl PMap PSet PwAff}:
   forall p, is_subset p p.
 Proof.
   move => p.
     by apply is_subset_spec.
 Qed.
 
-Theorem is_subset_trans `{PI: PresburgerImpl PSet PwAff}:
+Theorem is_subset_trans `{PI: PresburgerImpl PMap PSet PwAff}:
   forall p1 p2 p3, is_subset p1 p2 ->
               is_subset p2 p3 ->
               is_subset p1 p3.
@@ -136,7 +175,7 @@ Proof.
     by auto.
 Qed.
 
-Theorem is_subset_union_l `{PI: PresburgerImpl PSet PwAff} :
+Theorem is_subset_union_l `{PI: PresburgerImpl PMap PSet PwAff} :
   forall p1 p2, is_subset p1 (union_set p1 p2).
 Proof.
   move => p1 p2.
@@ -144,7 +183,7 @@ Proof.
     by rewrite union_set_spec Hp1.
 Qed.
 
-Theorem is_subset_union_r `{PI: PresburgerImpl PSet PwAff} :
+Theorem is_subset_union_r `{PI: PresburgerImpl PMap PSet PwAff} :
   forall p1 p2, is_subset p2 (union_set p1 p2).
 Proof.
   move => p1 p2.
@@ -152,24 +191,14 @@ Proof.
     by rewrite union_set_spec Hp2 orbT.
 Qed.
 
-(* Tactic to simplify proofs that use presburger sets or pw_aff *)
+Hint Rewrite @eval_pset_same @empty_set_spec_rw @universe_set_spec @union_set_spec @intersect_set_spec
+     @eval_pmap_same @empty_map_spec_rw @universe_map_spec @union_map_spec @intersect_map_spec
+     @pw_aff_from_aff_spec @intersect_domain_spec @union_pw_aff_spec @eq_set_spec @ne_set_spec @le_set_spec @indicator_function_spec : prw.
 
-Ltac simpl_presburger :=
-  repeat match goal with
-         | [ |- context[eval_set empty_set _]] => rewrite (negbTE (empty_set_spec _))
-         | [ |- context[eval_set universe_set _]] => rewrite universe_set_spec
-         | [ |- context[eval_set (union_set _ _) _]] => rewrite union_set_spec
-         | [ |- context[eval_set (intersect_set _ _) _]] => rewrite intersect_set_spec
-         | [ |- context[eval_set (set_project_out _ _) _]] => rewrite set_project_out_spec
-         | [ |- context[eval_pw_aff (pw_aff_from_aff _) _]] => rewrite pw_aff_from_aff_spec
-         | [ |- context[eval_pw_aff (intersect_domain _ _) _]] => rewrite intersect_domain_spec
-         | [ |- context[eval_pw_aff (union_pw_aff _ _) _]] => rewrite union_pw_aff_spec
-         | [ |- context[eval_set (eq_set _ _) _]] => rewrite eq_set_spec
-         | [ |- context[eval_set (ne_set _ _) _]] => rewrite ne_set_spec
-         | [ |- context[eval_set (le_set _ _) _]] => rewrite le_set_spec
-         | [ |- context[eval_pw_aff (indicator_function _) _]] => rewrite indicator_function_spec
-         | [ |- context[is_subset ?s ?s]] => rewrite is_subset_refl
-         | [ |- context[is_subset ?s (union_set ?s _)]] => rewrite is_subset_union_l
-         | [ |- context[is_subset ?s (union_set _ ?s)]] => rewrite is_subset_union_r
-         | _ => by autorewrite with totalrw
-         end.
+Hint Resolve @is_subset_spec @set_project_out_spec @is_subset_map_spec @map_project_out_spec.
+
+Ltac simpl_presburger_ := repeat (autorewrite with prw; simplssr).
+Ltac simpl_presburger := reflect_ne_in simpl_map_.
+
+Ltac auto_presburger := intros ; simpl_map; autossr.
+
