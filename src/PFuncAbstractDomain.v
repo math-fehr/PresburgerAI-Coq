e5de67d5a365261a8a0316db0831fd2799839526
mfehr <mathieu.fehr@gmail.com>
2020-01-13 14:33:10 +0100

    Add get_unioned_bot_set_pfuncmap, and definitions to prove its spec.

--- src/PFuncAbstractDomain.v
+++ src/PFuncAbstractDomain.v
@@ -1,7 +1,7 @@
 From Coq Require Import ssreflect ssrfun ssrbool.
 From Coq Require Import ssrbool Ensembles.
-From PolyAI Require Export PFunc LSSA AbstractDomain RelationalAbstractDomain.
 Local Set Warnings "-notation-overridden".
+From PolyAI Require Export PFunc LSSA AbstractDomain RelationalAbstractDomain ssrseq.
 From mathcomp Require Import ssrnat eqtype.
 
 Local Open Scope string_scope.
@@ -15,6 +15,78 @@ Module PFuncMap (FPI: FPresburgerImpl).
 
   Definition to_var_values_seq (p: Program) (R: RegisterMap) :=
     [ seq R v | v <- vars_in_program p ].
+
+  Fixpoint to_var_values_map_ (p: Program) (pairs: seq (string_eqType * Z)) :=
+    match pairs with
+    | [::] => TDefault 0%Z
+    | (k,v)::pairs' => (k !-> v; to_var_values_map_ p pairs')
+    end.
+
+  Theorem to_var_values_map_inbounds :
+    forall p pairs i,
+      uniq (unzip1 pairs) ->
+      i < size pairs ->
+      (to_var_values_map_ p pairs) (nth "" (unzip1 pairs) i) = nth 0%Z (unzip2 pairs) i.
+  Proof.
+    move => p. elim => [ // | ].
+    move => [k v] pairs Hind [|i] /= /andP[Hknotin Huniq] Hi; first by simpl_map.
+    rewrite t_update_neq. auto.
+    apply /eqP => Hk.
+    move => /negP in Hknotin. apply Hknotin.
+    rewrite Hk. apply mem_nth.
+    rewrite ltnS in Hi. by rewrite size_map.
+  Qed.
+
+  Theorem to_var_values_map_outbounds :
+    forall p keys values i,
+      i >= size values ->
+      i < size keys ->
+      uniq keys ->
+      (to_var_values_map_ p (zip keys values)) (nth "" keys i) = 0%Z.
+  Proof.
+    move => p. elim => [ [|k v] // | k keys Hind values i Hikeys Hivalues Huniq].
+    case: values Hikeys => [// | v values Hikeys].
+    case: i Hikeys Hivalues => [ // | i /= Hikeys Hivalues]. rewrite !ltnS in Hikeys Hivalues.
+    move: Huniq => /= /andP[Hkey Huniq].
+    rewrite /= t_update_neq; auto.
+    apply /eqP => Hknth. move => /negP in Hkey. apply Hkey.
+    eapply (mem_nth "") in Hivalues. by rewrite -Hknth in Hivalues.
+  Qed.
+
+  Definition to_var_values_map (p: Program) (s: seq Z) :=
+    let vars := vars_in_program p in
+    to_var_values_map_ p (zip vars s).
+
+
+  Theorem to_var_values_mapP :
+    forall p s i, i < size (vars_in_program p) ->
+             (to_var_values_map p s) (nth "" (vars_in_program p) i) =
+             nth 0%Z s i.
+  Proof.
+    move => p s i Hi.
+    move: (uniq_vars_in_program p) => Huniq.
+    move: (zip_uniq _ _ "" 0%Z (vars_in_program p) s Huniq) => Huniq_zip.
+    case Hsize: (i < size s).
+    - move: (to_var_values_map_inbounds p (zip (vars_in_program p) s) i).
+      rewrite /unzip1. have Huniq_map: (uniq [seq i.1 | i <- zip (vars_in_program p) s]).
+      + move Hsez: (unzip1 (zip (vars_in_program p) s)) => sez.
+        rewrite map_inj_in_uniq => [// | [x1 x2] [y1 y2] Hx Hy].
+        move: (index_uniq_zip _ _ _ _ _ _ Huniq Hx) => Hxindex.
+        move: (index_uniq_zip _ _ _ _ _ _ Huniq Hy) => Hyindex.
+        move => /= Heq. subst. rewrite -Hxindex in Hyindex.
+        rewrite -(nth_index ("",0%Z) Hx).
+        rewrite -(nth_index ("",0%Z) Hy).
+        by rewrite Hyindex.
+      + move => /(_ Huniq_map).
+        have: (i < size (zip (vars_in_program p) s)). rewrite size_zip leq_min. by autossr.
+        move => Hi' /(_ Hi'). rewrite /to_var_values_map.
+        rewrite (nth_map ("", 0%Z)) => //. rewrite nth_zip_cond Hi' /= => ->.
+        rewrite (nth_map ("", 0%Z)) => //. by rewrite nth_zip_cond Hi' /=.
+    - rewrite [in X in _ = X]nth_default; last first. rewrite leqNgt. by rewrite Hsize.
+      rewrite /to_var_values_map.
+      rewrite to_var_values_map_outbounds => //.
+      rewrite leqNgt. by rewrite Hsize.
+  Qed.
 
   Definition PFuncMap (p: Program) :=
     let n := size (vars_in_program p) in
@@ -45,7 +117,7 @@ Module PFuncMap (FPI: FPresburgerImpl).
 
   Definition gamma_seq_PFuncMap {p: Program} (pf: PFuncMap p) (x: seq Z * seq Z) :=
     let n := size (vars_in_program p) in
-    forall i, i < n -> nth 0%Z x.2 i \inV (eval_pfunc (nth (constant_pfunc _ VTop) (sval pf) i) x.1).
+    all (fun i => nth 0%Z x.2 i \inV (eval_pfunc (nth (constant_pfunc _ VTop) (sval pf) i) x.1)) (iota 0 n).
 
   Definition gamma_PFuncMap {p: Program} (pf: PFuncMap p) (x: concrete_state) :=
     gamma_seq_PFuncMap pf (to_var_values_seq p x.1, to_var_values_seq p x.2).
@@ -53,69 +125,44 @@ Module PFuncMap (FPI: FPresburgerImpl).
   Theorem gamma_top_PFuncMap :
     forall p x, Ensembles.In _ (gamma_PFuncMap (top_PFuncMap p)) x.
   Proof.
-    move => p x n i Hibounds. rewrite nth_nseq. rewrite Hibounds.
-    by auto_pfunc.
+    move => p x. rewrite /In /gamma_PFuncMap /gamma_seq_PFuncMap /=.
+    apply /allP => i Hi. rewrite nth_nseq. rewrite mem_iota in Hi. move: Hi. move => /andP[_ Hi].
+    rewrite add0n in Hi. rewrite Hi /eval_pfunc.
+      by auto_pfunc.
   Qed.
 
   Theorem gamma_bot_PFuncMap :
     forall p x, ~ Ensembles.In _ (gamma_PFuncMap (bot_PFuncMap p)) x.
   Proof.
-    rewrite /In /gamma_PFuncMap /bot_PFuncMap => p x /(_ 0).
-    rewrite size_vars_in_program => /(_ is_true_true).
-    rewrite nth_nseq.
-    case_if. by simpl_pfunc.
-      by rewrite size_vars_in_program in H.
+    rewrite /In /gamma_PFuncMap /gamma_seq_PFuncMap /bot_PFuncMap => p x /allP /(_ 0).
+    have Hiniota: (0 \in iota 0 (size (vars_in_program p))). rewrite mem_iota. apply /andP. split; auto. rewrite add0n. apply size_vars_in_program.
+    move => /(_ Hiniota). rewrite nth_nseq. rewrite size_vars_in_program.
+    by simpl_pfunc.
   Qed.
 
-    Theorem join_sound_l_PFuncMap :
-      forall p a1 a2, Included _ (@gamma_PFuncMap p a1) (gamma_PFuncMap (join_PFuncMap a1 a2)).
-    Proof.
-      rewrite /Included /In /gamma_PFuncMap /join_PFuncMap => p [a1 size_a1] [a2 size_a2].
-      move: (size_a1) (size_a2).
-      move => /eqP in size_a1. move => /eqP in size_a2.
-      move => size_a1' size_a2' x Hle n i.
-      move => /(_ i) in Hle. move: Hle. simpl_pfunc.
-      move => Htemp Hibounds. move: (Htemp Hibounds).
-      apply le_V_spec.
-      case Hs : (i < size a1).
-      - erewrite nth_map; last first. by rewrite size_zip size_a1 size_a2 minnn -size_a1.
-        erewrite nth_zip; last first. by rewrite size_a1 size_a2.
-        rewrite /= join_pfuncP.
-          by apply join_V_leftP.
-      - rewrite !nth_default.
-        + by apply le_V_refl.
-        + rewrite size_map size_zip size_a1 size_a2 minnn -size_a1.
-            by rewrite leqNgt Hs.
-        + by rewrite leqNgt Hs.
-    Unshelve.
-      eapply constant_pfunc.
-      by apply VTop.
-    Qed.
+  Theorem join_sound_l_PFuncMap :
+    forall p a1 a2, Included _ (@gamma_PFuncMap p a1) (gamma_PFuncMap (join_PFuncMap a1 a2)).
+  Proof.
+    rewrite /Included /In /gamma_PFuncMap /gamma_seq_PFuncMap /join_PFuncMap /= => p [a1 size_a1] [a2 size_a2] x.
+    apply sub_all_in => i /=. case Hin: (i \in iota _ _) => //. rewrite !implyTb.
+    erewrite nth_map. erewrite nth_zip. rewrite /= join_pfuncP. apply le_V_spec. apply join_V_leftP.
+    by move: size_a1 size_a2 => /eqP -> /eqP ->.
+    rewrite size_zip. move: size_a1 size_a2 => /eqP -> /eqP ->. rewrite minnn.
+    move: Hin. rewrite mem_iota => /andP[_]. by rewrite add0n.
+    Unshelve. eapply constant_pfunc. apply VTop.
+  Qed.
 
-    Theorem join_sound_r_PFuncMap :
-      forall p a1 a2, Included _ (@gamma_PFuncMap p a2) (gamma_PFuncMap (join_PFuncMap a1 a2)).
-    Proof.
-      rewrite /Included /In /gamma_PFuncMap /join_PFuncMap => p [a1 size_a1] [a2 size_a2].
-      move: (size_a1) (size_a2).
-      move => /eqP in size_a1. move => /eqP in size_a2.
-      move => size_a1' size_a2' x Hle n i.
-      move => /(_ i) in Hle. move: Hle. simpl_pfunc.
-      move => Htemp Hibounds. move: (Htemp Hibounds).
-      apply le_V_spec.
-      case Hs : (i < size a2).
-      - erewrite nth_map; last first. by rewrite size_zip size_a1 size_a2 minnn -size_a2.
-        erewrite nth_zip; last first. by rewrite size_a1 size_a2.
-        rewrite /= join_pfuncP.
-          by apply join_V_rightP.
-      - rewrite !nth_default.
-        + by apply le_V_refl.
-        + rewrite size_map size_zip size_a1 size_a2 minnn -size_a2.
-            by rewrite leqNgt Hs.
-        + by rewrite leqNgt Hs.
-          Unshelve.
-          eapply constant_pfunc.
-            by apply VTop.
-    Qed.
+  Theorem join_sound_r_PFuncMap :
+    forall p a1 a2, Included _ (@gamma_PFuncMap p a2) (gamma_PFuncMap (join_PFuncMap a1 a2)).
+  Proof.
+    rewrite /Included /In /gamma_PFuncMap /gamma_seq_PFuncMap /join_PFuncMap /= => p [a1 size_a1] [a2 size_a2] x.
+    apply sub_all_in => i /=. case Hin: (i \in iota _ _) => //. rewrite !implyTb.
+    erewrite nth_map. erewrite nth_zip. rewrite /= join_pfuncP. apply le_V_spec. apply join_V_rightP.
+    by move: size_a1 size_a2 => /eqP -> /eqP ->.
+    rewrite size_zip. move: size_a1 size_a2 => /eqP -> /eqP ->. rewrite minnn.
+    move: Hin. rewrite mem_iota => /andP[_]. by rewrite add0n.
+    Unshelve. eapply constant_pfunc. apply VTop.
+  Qed.
 
     Definition PFuncMap_eqMixin (p: Program) :=
       let n := size (vars_in_program p) in
@@ -147,41 +194,64 @@ Module PFuncMap (FPI: FPresburgerImpl).
     forall x, In _ (gamma_PFuncMap (id_relation_PFuncMap p)) (x, x).
   Proof.
     move => x.
-    rewrite /gamma_PFuncMap /In /= => i n Hibound.
-    rewrite nth_mkseq => [ | //].
+    rewrite /gamma_PFuncMap /In /gamma_seq_PFuncMap /=.
+    apply /allP => i. rewrite mem_iota add0n => /andP[_ Hi]. rewrite nth_mkseq => [ | //].
     auto_pfunc.
   Qed.
 
-  Definition get_intersected_assumed_set_aux {n: nat} (pf: seq (PFunc n)) (base: PSet n) : PSet n :=
-    foldl (fun acc val => f_intersect_set acc (Assumed val)) base pf.
-
   Definition get_intersected_assumed_set {n: nat} (pf: seq (PFunc n)) : PSet n :=
-    get_intersected_assumed_set_aux pf (f_universe_set n).
-
-  Theorem get_intersected_assumed_set_base :
-    forall n (pf: seq (PFunc n)) base,
-      f_is_subset_set (get_intersected_assumed_set_aux pf base) base.
-  Proof.
-    move => n. elim. by auto_finite_presburger.
-    move => p pf Hind base /=.
-    eapply f_is_subset_set_trans. by apply Hind.
-      by autossr.
-  Qed.
+    foldl (fun acc val => f_intersect_set acc val) (f_universe_set _) [seq Assumed val | val <- pf].
 
   Theorem get_intersected_assumed_setP :
-    forall n (pf: seq (PFunc n)),
-      forall i, i < size pf ->
-           forall default base, let pf_i := nth default pf i in
-                f_is_subset_set (get_intersected_assumed_set_aux pf base) (Assumed pf_i).
+    forall n pf x, x \ins (@get_intersected_assumed_set n pf) = all (fun pfunc => x \ins (Assumed pfunc)) pf.
   Proof.
-    move => n. elim => [ // | ].
-    move => p pf Hind [|i] Hi default base /=.
-    - rewrite /get_intersected_assumed_set_aux /=.
-      eapply f_is_subset_set_trans. by apply get_intersected_assumed_set_base.
-        by auto_finite_presburger.
-    - apply Hind.
-      by rewrite /= -[in x in _ < x]addn1 -[i.+1]addn1 ltn_add2r in Hi.
+    move => n pf x. rewrite /get_intersected_assumed_set.
+    pose A := fun (s: PSet n) => (x \ins s).
+    have HA : forall s, A s = (x \ins s). by rewrite /A.
+    rewrite -HA fold_intersection.
+    - rewrite /A. simpl_pfunc. by rewrite all_map /=.
+    - move => acc a. rewrite /A. by simpl_pfunc.
   Qed.
+
+  Definition get_unioned_bot_set {n: nat} (pf: seq (PFunc n)) :=
+    foldl (fun acc val => f_union_set acc val) (f_empty_set _) [seq pfunc_get_bot_set val | val <- pf ].
+
+  Theorem get_unioned_bot_setP :
+    forall n pf x, x \ins (@get_unioned_bot_set n pf) = has (fun pfunc => x \ins (pfunc_get_bot_set pfunc)) pf.
+  Proof.
+    move => n pf x.
+    pose A := fun (s: PSet n) => (x \ins s).
+    have HA : forall s, A s = (x \ins s). by rewrite /A.
+    rewrite -HA fold_union /=.
+    - rewrite /A. simpl_pfunc. by rewrite has_map.
+    - move => acc a. rewrite /A. by simpl_pfunc.
+  Qed.
+
+  Theorem get_unioned_bot_set_pfuncmapP :
+    forall p pf x_in, (to_var_values_seq p x_in) \ins (get_unioned_bot_set (sval pf)) <-> forall x_out, ~ (In _ (@gamma_PFuncMap p pf) (x_in, x_out)).
+  Proof.
+    move => p [x_pf H_pf] x_in /=. rewrite /In /gamma_PFuncMap /gamma_seq_PFuncMap /=. move => /eqP in H_pf.
+    set top_pfunc := {| Val := _ |}.
+    split => [ HIn x_out /= | HIn ].
+    - apply /negP /allPn.
+      move: HIn. rewrite get_unioned_bot_setP => /hasP [pfunc /nthP Hpfunc_in x_in_in].
+      move => /(_ top_pfunc) in Hpfunc_in. move: Hpfunc_in => [i [Hi Hnth]].
+      exists i. rewrite mem_iota. apply /andP. split; auto. by rewrite add0n -H_pf.
+      rewrite Hnth.
+      rewrite pfunc_get_bot_setP in x_in_in. move => /eqP in x_in_in. by rewrite x_in_in.
+    - rewrite get_unioned_bot_setP. apply /hasP.
+      set x_out := ([ seq match eval_pfunc pfunc (to_var_values_seq p x_in) with | VTop | VBot => 0%Z | VVal v => v end | pfunc <- x_pf ]).
+      set x_out_map := to_var_values_map p x_out.
+      move: HIn => /(_ x_out_map) /negP /allPn [i Hi Hnotin].
+      move: Hi. rewrite mem_iota => /andP[_ Hi]. rewrite add0n in Hi.
+      rewrite {1}/to_var_values_seq (nth_map "") in Hnotin => //.
+      rewrite /x_out_map to_var_values_mapP /x_out in Hnotin => //.
+      erewrite (nth_map top_pfunc) in Hnotin; last by rewrite H_pf.
+      move: Hnotin. case Heval: (eval_pfunc _ _) => [ // | n | ]. by rewrite /in_V eq_refl.
+      move => _. exists (nth top_pfunc x_pf i). apply mem_nth. by rewrite H_pf.
+        by rewrite pfunc_get_bot_setP Heval.
+  Qed.
+
 
   Program Definition pfuncmap_to_map {p: Program} (pf: PFuncMap p) :
     PMap (size (vars_in_program p)) (size (vars_in_program p)) :=
@@ -206,13 +276,13 @@ Module PFuncMap (FPI: FPresburgerImpl).
     rewrite /pfuncmap_to_map f_cast_mapP f_concat_mapP => i Hi.
     rewrite (nth_map top_pfunc); last first. by rewrite Hpf_eq.
     rewrite f_map_from_pw_affP /=.
-    rewrite /gamma_seq_PFuncMap /= in Hgamma. move => /(_ i Hi) in Hgamma. rewrite /eval_pfunc in Hgamma.
-    move: (get_intersected_assumed_setP n pf i).
-    rewrite Hpf_eq => /(_ Hi top_pfunc (f_universe_set n)) /= => Hle.
-    move => /f_is_subset_setP /(_ x_in Hintersected) in Hle.
-    rewrite Hle in Hgamma. move: Hgamma.
-    case_match => [ v | //].
-      by rewrite /= => /eqP ->.
+    rewrite /gamma_seq_PFuncMap /= /allP in Hgamma.
+    have Hiota : (i \in iota 0 (size (vars_in_program p))). rewrite mem_iota. apply /andP => //.
+    move => /allP /(_ i Hiota) in Hgamma. rewrite /eval_pfunc in Hgamma.
+    rewrite (get_intersected_assumed_setP n pf x_in) in Hintersected.
+    move => /allP in Hintersected. move: Hgamma.
+    rewrite Hintersected. case_match => [ v /= /eqP -> // | //].
+    apply mem_nth. by rewrite Hpf_eq.
   Qed.
 
   Theorem is_single_valued_pfunc_map_to_map :
@@ -235,6 +305,7 @@ Module PFuncMap (FPI: FPresburgerImpl).
       by apply (constant_pfunc (size (vars_in_program p)) VTop).
   Qed.
 
+  (*
   Program Definition compose_relation_PFuncMap {p: Program} (pf1 pf2: PFuncMap p) : PFuncMap p :=
     let map1 := pfuncmap_to_map pf1 in
     let new_val := fun pf => f_apply_map_to_pw_aff map1 (is_single_valued_pfunc_map_to_map p pf1) (Val pf) in
@@ -244,78 +315,7 @@ Module PFuncMap (FPI: FPresburgerImpl).
     [ seq (mkPFunc (new_val pf) (new_assumed pf)) | pf <- sval pf2 ].
   Next Obligation.
     rewrite size_map. by case pf2 => //.
-  Defined.
-
-  Theorem compose_relation_PFuncMapP :
-    forall p x1 x0 x2 (pf1 pf2: PFuncMap p),
-      In _ (gamma_PFuncMap pf1) (x0, x1) ->
-      In _ (gamma_PFuncMap pf2) (x1, x2) ->
-      In _ (gamma (compose_relation_PFuncMap pf1 pf2)) (x0, x2).
-  Proof.
-    move => p x1 x0 x2 pf1 pf2.
-    move => /= Hgamma01 Hgamma12. move: (Hgamma01) (Hgamma12).
-    case Hpf1: pf1 => [x_pf1 H_pf1].
-    case Hpf2: pf2 => [x_pf2 H_pf2].
-    rewrite /In /= /gamma_PFuncMap.
-    rewrite /In /gamma_PFuncMap /= in Hgamma01 Hgamma12.
-    set x0' := (to_var_values_seq p x0).
-    set x1' := (to_var_values_seq p x1).
-    set x2' := (to_var_values_seq p x2).
-    rewrite /gamma_seq_PFuncMap /=.
-    set top_pfunc := {| Val := _; Assumed := _ |}.
-    move => H01 H12 i Hi.
-    move => /(_ i Hi) in H01. move => /(_ i Hi) in H12.
-    erewrite nth_map; last first. by move: (H_pf2) => /eqP ->.
-    rewrite /eval_pfunc /= in H01 H12 *. simpl_pfunc.
-    move: H01. case H0Assumed: (x0' \ins _); last first.
-    - have /negb_true_iff -> : (~~ (x0' \ins get_intersected_assumed_set x_pf1)) => [ | //].
-      apply /negP => Hin.
-      move: (get_intersected_assumed_setP (size (vars_in_program p)) x_pf1 i).
-      move: (H_pf1) => /eqP ->. move => /(_ Hi top_pfunc (f_universe_set (size (vars_in_program p)))) /=.
-      rewrite f_is_subset_setP => /(_ x0' Hin).
-        by rewrite H0Assumed.
-    - case H0: (f_eval_pw_aff _ _) => [ v0 | ] => [ Hv0 | //].
-      case Hx0'assumed: (x0' \ins get_intersected_assumed_set x_pf1) => [ /= | //].
-      case_if => [ | // ].
-      apply f_get_domain_mapP in H.
-      case: H => x1'same. simpl_pfunc => /andP[Hevalx0_same Hout_in_assumed].
-      move: (pfuncmap_to_mapP p pf1 x0' x1').
-      move => /(_ Hgamma01).
-      erewrite f_apply_map_to_pw_affP; last by apply Hevalx0_same.
-      rewrite [in sval _]Hpf1 /= Hx0'assumed /= => Hevalx0.
-      rewrite -Hpf1 in Hevalx0_same.
-      move: (is_single_valued_pfunc_map_to_map p pf1) => /f_is_single_valued_mapP.
-      move => /(_ x0' x1'same x1' Hevalx0_same Hevalx0) Heqx1'.
-      erewrite f_eval_pset_same in Hout_in_assumed; eauto.
-      rewrite Hout_in_assumed in H12.
-        by erewrite f_eval_pw_aff_same; eauto.
-  Qed.
-
-  Definition compose_relation_with_bot_PFuncMap {p: Program} (pf1 pf2: PFuncMap p) : PFuncMap p :=
-    if (pf2 == bot_PFuncMap p) then
-      bot_PFuncMap p
-    else
-      compose_relation_PFuncMap pf1 pf2.
-
-  Theorem compose_relation_with_bot_PFuncMapP :
-    forall p x1 x0 x2 (pf1 pf2: PFuncMap p),
-      In _ (gamma_PFuncMap pf1) (x0, x1) ->
-      In _ (gamma_PFuncMap pf2) (x1, x2) ->
-      In _ (gamma (compose_relation_with_bot_PFuncMap pf1 pf2)) (x0, x2).
-  Proof.
-    move => p x1 x0 x2 pf1 pf2 Hin01 Hin12.
-    rewrite /compose_relation_with_bot_PFuncMap.
-    case_if.
-    - move => /eqP in H. rewrite H in Hin12. by move: (gamma_bot_PFuncMap p (x1, x2)).
-    - by eapply compose_relation_PFuncMapP; eauto.
-  Qed.
-
-  Theorem compose_relation_bot_PFuncMap :
-    forall p x1, compose_relation_with_bot_PFuncMap x1 (bot_PFuncMap p) = (bot_PFuncMap p).
-  Proof.
-    move => p x1. rewrite /compose_relation_with_bot_PFuncMap.
-      by rewrite eq_refl.
-  Qed.
+  Defined. *)
 
   (* Fail Instance adom_relational_pmap (p: Program) : adom_relational (adom_pmap p) :=
     {
